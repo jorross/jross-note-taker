e63f2bb882694eb5ad2106075cada4e1c6c4ecd6
Jordan Ross <jorross@umich.edu>
2022-04-02 10:43:14 -0700

    adding environment port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const noteData = require("./db/db.json");
 // Helper method for generating unique ids
 const uuid = require("./helpers/uuid");
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
